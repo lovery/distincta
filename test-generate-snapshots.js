ced863c12da116cca07ac35fd48ed562c09d6004
Zhivka Dimova <ji@magicsolutions.bg>
2017-02-05 14:37:06 +0200

    Add files for test enviroment

---
+++ test-generate-snapshots.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var util = require("util");
+var assert = require("assert");
+var source = "test-sitemap.xml";
+var htmlSnapshots = require('html-snapshots');
+
+if (process.argv.length >= 3) {
+	source = process.argv[2];
+}
+
+var result = htmlSnapshots.run({
+	input: "sitemap",
+	source: source,
+	outputDir: "./snapshots",
+	outputDirClean: true,
+	useJQuery: true,
+	timeout: 20000
+}, function(err, completed) {
+
+	console.log("completed snapshots:");
+	console.log(util.inspect(completed));
+	// throw if there was an error
+	assert.ifError(err);
+});
