c8656db27c931fa2d8066c8f73eeafed23c929e0
Zhivka Dimova <ji.gancheva@gmail.com>
2019-03-06 09:31:10 +0100

    Reorder function params

--- app/controllers/NewsItemCtrl.js
+++ app/controllers/NewsItemCtrl.js
@@ -5,22 +5,29 @@
 		'$scope', '$stateParams', '$rootScope', '$http', '$location', '$window', 'ngMeta',
 		function ($scope, $stateParams, $rootScope, $http, $location, $window, ngMeta) {
 			$http.get('data_files/news/' + $stateParams.lang + '_' + $stateParams.news_item + '.json')
-				.then(function (response) {
-					if (typeof data != "string") {
-						$scope.news_data = response.data[0];
+				.then(
+					// success handler
+					function (response) {
+						if (typeof data != "string") {
+							$scope.news_data = response.data[0];
 
-						ngMeta.setTitle($scope.news_data.page_title, '');
-						for (var tag in $scope.news_data.meta) {
-							if (tag.endsWith(':image')) {
-								ngMeta.setTag(tag, $location.imageOrigin + 'news/' + $scope.news_data.meta[tag]);
-							} else {
-								ngMeta.setTag(tag, $scope.news_data.meta[tag]);
+							ngMeta.setTitle($scope.news_data.page_title, '');
+							for (var tag in $scope.news_data.meta) {
+								if (tag.endsWith(':image')) {
+									ngMeta.setTag(tag, $location.imageOrigin + 'news/' + $scope.news_data.meta[tag]);
+								} else {
+									ngMeta.setTag(tag, $scope.news_data.meta[tag]);
+								}
 							}
+						} else {
+							$window.location.href = '/' + $stateParams.lang + '/home';
 						}
-					} else {
-						$window.location.href = '/' + $stateParams.lang + '/home';
+					},
+					// error handler
+					function (error) {
+						$window.location.href = '/' + $stateParams.lang + '/news';
 					}
-				});
+				);
 			$('#distincta-nav-bar').addClass('scrolled');
 			allowScrollMenu = false;
 			loadAllAnimation('.news');
