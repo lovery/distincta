3868b0888faeeb84b43335afb099e2b9f5bfac83
Zhivka Dimova <ji.gancheva@gmail.com>
2019-04-05 22:26:05 +0200

    Restore accordeon function for expand and collapse

--- app/controllers/SectionCtrl.js
+++ app/controllers/SectionCtrl.js
@@ -20,6 +20,17 @@
 			$('#distincta-nav-bar').addClass('scrolled');
 			allowScrollMenu = false;
 			loadAllAnimation('.section');
+			$scope.accordeonService = function (clickedOrder) {
+				if ($('#collapse_'+clickedOrder).children().hasClass('active')) {
+					$('#collapse_'+clickedOrder).children().removeClass('active');
+					$('#fa-icon_active_'+clickedOrder).addClass('hidden');
+					$('#fa-icon_inactive_'+clickedOrder).removeClass('hidden');
+				} else {
+					$('#collapse_'+clickedOrder).children().addClass('active');
+					$('#fa-icon_inactive_'+clickedOrder).addClass('hidden');
+					$('#fa-icon_active_'+clickedOrder).removeClass('hidden');
+				}
+			};
 		}
 	]);
 }());
