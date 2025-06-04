680d8fa6b2eba90cd53e649d49477104ad2c9805
Zhivka Dimova <zhivka.dimova@myforest.net>
2025-06-04 10:02:52 +0200

    Fix some accessibility issues

--- src/en/about/_data.js
+++ src/en/about/_data.js
@@ -1,27 +1,27 @@
 const { globals, methods } = require('../_data_preparation.js');
 
 var people = [
-    {
-        "slug": "ralitsa-dimova",
-		"name": "Ralitsa Dimova",
-        "position": "",
-        "hideCommonHeadline": true,
-		"photo": "ralitsa.png",
-		"alt_text": "Ralitsa Dimova Photo intellectual property attorney register trade marks, patents in Bulgaria, Europe and USA",
-		"bg_trade_number": "Bulgarian Trade Mark and Design Attorney <span class=\"nowrap\">No. 301</span>",
-		"en_trade_number": "European Trade Mark and Design Attorney <span class=\"nowrap\">No. 39624</span>",
-		"meta": {
-			"description": "Ralitsa Dimova intellectual property attorney, representation before BPO, OHIM, WIPO, customs authorities, the Court",
-			"keywords": "Intellectual Property, Trade Marks, Designs, Copyright, Patents, Geographical Indications, Trade Secrets, Registration, Certificate, License, Agreement, Innovation, Creative, Litigation, Bulgarian Patent Office, OHIM, WIPO, EPO"
-		},
-        "content": [],
-		"bio": [
-			"Ralitsa holds a Master’s Degree in Intellectual Property.",
-			"She has been working in the field of intellectual proprties since 2001.",
-			"Since 2007 she has been a licensed trade mark and design attorney, representing clients before the Bulgarian Patent Office, the European Union Intellectual Property Office and the World Intellectual Property Organization, consulting them on the most time and cost-efficient way to register intellectual property's rights in Bulgaria and abroad.",
-			"She has plenty of experience handling large intellectual property portfolios strategizing on the most effective protection and enforcement, negotiating and concluding contracts and coexistence agreements. She is an active participant in national and international forums on any subject that concerns the development of the intellectual property legislative framework and practice."
-        ]
-    }
+  {
+    "slug": "ralitsa-dimova",
+    "name": "Ralitsa Dimova",
+    "position": "",
+    "hideCommonHeadline": true,
+    "photo": "ralitsa_235px.webp",
+    "alt_text": "Ralitsa Dimova Photo intellectual property attorney register trade marks, patents in Bulgaria, Europe and USA",
+    "bg_trade_number": "Bulgarian Trade Mark and Design Attorney <span class=\"nowrap\">No. 301</span>",
+    "en_trade_number": "European Trade Mark and Design Attorney <span class=\"nowrap\">No. 39624</span>",
+    "meta": {
+      "description": "Ralitsa Dimova intellectual property attorney, representation before BPO, OHIM, WIPO, customs authorities, the Court",
+      "keywords": "Intellectual Property, Trade Marks, Designs, Copyright, Patents, Geographical Indications, Trade Secrets, Registration, Certificate, License, Agreement, Innovation, Creative, Litigation, Bulgarian Patent Office, OHIM, WIPO, EPO"
+    },
+    "content": [],
+    "bio": [
+      "Ralitsa holds a Master’s Degree in Intellectual Property.",
+      "She has been working in the field of intellectual proprties since 2001.",
+      "Since 2007 she has been a licensed trade mark and design attorney, representing clients before the Bulgarian Patent Office, the European Union Intellectual Property Office and the World Intellectual Property Organization, consulting them on the most time and cost-efficient way to register intellectual property's rights in Bulgaria and abroad.",
+      "She has plenty of experience handling large intellectual property portfolios strategizing on the most effective protection and enforcement, negotiating and concluding contracts and coexistence agreements. She is an active participant in national and international forums on any subject that concerns the development of the intellectual property legislative framework and practice."
+    ]
+  }
 ];
 
 var pages_data = {
